a346f0b934775d0d5817717a9ff93e41bae51a2d
AntonioMarban <393tony393@gmail.com>
2023-10-27 09:50:02 -0600

    debuggeando

--- PalindromoPilas.cpp
+++ PalindromoPilas.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 #include "TStack.h"
 #include "Pilas1.cpp"
 using namespace std;
@@ -36,23 +37,24 @@ int main() {
 	 char cadena[MAX];
 	 char cadenaLimpia[MAX];
    
-   cin.gets(cadena);
+   cin.get(cadena, MAX);
    
    limpiaCadena(cadena, cadenaLimpia); // elimina signos de puntuación y cambia acentos     //no que un establo
    char *p1=cadenaLimpia, *p2=&cadenaLimpia[strlen(cadenaLimpia)-1];
    TStack <char> *pila1=new TStack<char>();
    TStack <char> *pila2=new TStack<char>();
    while ( *p1 ) {
-           pila1->push(*p1);
-           pila2->push(*p2);
+           pila1->push(p1);
+           pila2->push(p2);
            p1++; p2--;
     } 
-	  while (!pila1->empty() && !(pila2->empty()) {
+	  while (!pila1->empty() && !(pila2->empty())) {
 	     if (pila1->pop()!=pila2->pop())
             break;
     } 
     if (pila1->empty() && pila2->empty())
-         cout << “La cadena es palíndromo”;
-    else 
-         cout << “La cadena no es palíndromo”; 
+        cout << "La cadena es palíndromo";
+    else {
+      cout << "La cadena no es palíndromo";
+    }
 }
--- main.cpp
+++ main.cpp
@@ -10,37 +10,57 @@ using namespace std;
 #define MAX 200
 
 int main() {
-    char expression[MAX]; // Creates string variable that will store the mathematic expression
+    char expression[MAX] = "{A+B + (a+b)}"; // Creates string variable that will store the mathematic expression
     bool valid = true; // Boolean variable that checks if our expression is valid
    
-    cin.get(expression); // Receives string input and stores it in the expression variable
+    cout << "Please enter a mathematic expression to validate its parentheses use." << endl;
+    cout << ">>>";
 
-    char *expressionPointer=expression; // Makes a char type pointer that points to the expression variable
+    //  cin.get(expression, MAX); // Receives string input and stores it in the expression variable
 
-    TStack <char> *stack=new TStack<char>(); // Creates stack object
+    char * expressionPointer = expression; // Makes a char type pointer that is set to the expression which is a char[]
+    cout << *expressionPointer << "      *expressionPointer" << endl;
+    cout << expressionPointer << "     expressionPointer" << endl;
+
+    TStack <char> *stack = new TStack<char>(); // Creates stack object
+
+    int counter = 0; // counter that iterates over expression
 
     while (*expressionPointer) { // Traverses our string
+        cout << *expressionPointer;
         if (*expressionPointer == '(' || *expressionPointer == '[' || *expressionPointer == '{') { // If it encounters opening symbol
-            stack->push(expressionPointer); // Pushes it into our stack
+            stack->push(*expressionPointer); // ERROR HERE argument of type "char" is incompatible with parameter of type "char *"
+            cout << "\n========" << stack->stacktop() << "=====\n";
         }
         if (*expressionPointer == ')' || *expressionPointer == ']' || *expressionPointer == '}') { // If it encounters closing symbol
             if (stack->empty()) { // Checks if our stack is empty
                 valid = false; // If it is empty, then it is an invalid expression because there is no opening symbol to match it
             }
             else { // If it is not empty
+                cout << "pila no vacia";
+                cout << "\n========" << stack->stacktop() << "=====\n";
                 char *popped = stack->pop(); // Pops the top element
+                cout << "llegue aqui";
+                cout << *popped;
                 switch(*expressionPointer) { 
-                    case ')': if (*popped != '(') {valid = false;}; break; // If the closing symbol is a ')' but the popped element isn't a '('
-                    case ']': if (*popped != '[') {valid = false;}; break; // it becomes an invalid expression
-                    case '}': if (*popped != '{') {valid = false;}; break; // Same process for all the other symbols
+                    case ')': if (*popped != '(') {valid = false;} // If the closing symbol is a ')' but the popped element isn't a '('
+                    break; 
+                    case ']': if (*popped != '[') {valid = false;} // it becomes an invalid expression
+                    break;
+                    case '}': if (*popped != '{') {valid = false; cout << "it's a {}";} // Same process for all the other symbols
+                    break; 
                 }
             }
         }
+        expressionPointer++;
+        cout << counter << endl;
     }
+    cout << "\nWhile finished" << endl;
     if (!stack->empty()) { // If by the time we finish traversing the string, our stack is NOT empty
         valid = false; // Then it is an invalid expression, because there is an opening symbol waiting to be closed (and therefore, popped)
     }
-
+    cout << "Empty checked" << endl;
+    cout << endl;
     if (valid) {
         cout << "Valid expression" << endl; // If valid, valid
     }
